79f56da58f0f163789e17fab23b547ccb302ae70
victor@victor <victor.dmaina@gmail.com>
2024-03-14 19:26:40 +0300

    add

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+
+    input.addEventListener('click', clickAlert);
 }
+
+const button = document.createElement('button');
+button.id = 'button';
+document.body.appendChild(button);
+
+addingEventListener();
+
+const eventListeners = button.onclick;
+const isEventListenerAdded = typeof eventListeners === 'function';
+
+console.log("Is event listener added?", isEventListenerAdded);
